14b912bee646795191e05de1a46bd53dc84eb8c2
TAi <daudangvua@gmail.com>
2025-03-28 08:49:01 +0700

    add .

---
+++ card.js
@@ -0,0 +1,13 @@
+class Card {
+    width;
+    height;
+    color;
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    turn() {
+
+    }
+}
